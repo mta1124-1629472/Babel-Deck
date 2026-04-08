34607a659b5e52629cf3194282bac9b91ffdadf6
mta1124-1629472 <32500316+mta1124-1629472@users.noreply.github.com>
2026-04-08 01:21:58 -0700

    refactor: rename FlushPendingSave back to SaveCurrentSession in SessionWorkflowCoordinatorUnitTests

--- BabelPlayer.Tests/SessionWorkflowCoordinatorUnitTests.cs
+++ BabelPlayer.Tests/SessionWorkflowCoordinatorUnitTests.cs
@@ -1184,7 +1184,7 @@ public sealed class SessionWorkflowCoordinatorUnitTests : IDisposable
     {
         var coord = CreateCoordinator();
         coord.Initialize();
-        coord.FlushPendingSave();
+        coord.SaveCurrentSession();
 
         var coord2 = CreateCoordinator();
         coord2.Initialize();
